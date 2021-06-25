5f1414085d79df3edce3522511ad3f36a44a8916
thornbug90 <crazydev0409@gmail.com>
2021-06-25 17:12:53 +0200

    fixed isPrime.js

--- src/extraFn/isPrime.js
+++ src/extraFn/isPrime.js
@@ -10,6 +10,6 @@ const isPrime = (num) => {
   return true;
 };
 
-const sayPrimeOrNot = (num) => (isPrime(num) ? 'yes' : 'no');
+const sayPrimeOrNot = (num) => (isPrime(num) ? console.log('yes') : console.log('no'));
 
 export default sayPrimeOrNot;
